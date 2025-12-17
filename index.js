09e6700fa5bfebb822abe5b81be30002b78a259f
Voronskoy <marskoy27@gmail.com>
2025-12-17 18:58:29 +0200

    node:os

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import chalk from 'chalk';
-import getRandomCompliment from "./compliments.mjs";
-
+import os from "node:os";
+import chalk from "chalk";
+import getRandomCompliment from "./compliments.js";
 
 console.log(chalk.bgBlue(getRandomCompliment()));
+console.log(chalk.bgGreen(os.userInfo().username));
